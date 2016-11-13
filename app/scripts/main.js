2be1c1d7b9f8a11d5a16436335d2340a9d50ec27
Andrei Ivanov <z.shpack@gmail.com>
2016-11-12 23:07:27 -0800

    intro page work in progress

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,11 +5,19 @@
     var thisYear = date.getFullYear();
     $('.js-year').html(thisYear);
 
+    $('[data-typer-targets]').typer();
+
     //Intro section - Plax effect
     $('.intro-image').plaxify({'xRange':10,'yRange':20});
 	$('.intro-image-2').plaxify({'xRange':7,'yRange':10});
     $('.intro-image-3').plaxify({'xRange':10,'yRange':15});
     $.plax.enable();
+
+    $(document).scroll(function() {
+        var pos = $(document).scrollTop();
+        var parallax = parseInt(pos * -0.3) + 'px';
+        $('.js-intro-title').css('margin-top', parallax);
+    });
 
 
 
