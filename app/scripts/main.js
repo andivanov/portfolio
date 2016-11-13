54125fb7c1b6d1c41711c65ef09ecc8633d8c0bb
Andrei Ivanov <z.shpack@gmail.com>
2016-11-13 14:41:43 -0800

    added feature: fade in element on scroll

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -28,4 +28,28 @@
         }
     });
 
+
+    //Fade in elements
+    var element = $('.js-fadeInElement');
+    $(element).addClass('js-fade-element-hide');
+    $(window).scroll(function() {
+        for (let i = 0; i < element.length; i++) {
+            let elementTopToPageTop = $(element[i]).offset().top;
+            let windowTopToPageTop = $(window).scrollTop();
+            let windowInnerHeight = window.innerHeight;
+            let elementTopToWindowTop = elementTopToPageTop - windowTopToPageTop;
+            let elementTopToWindowBottom = windowInnerHeight - elementTopToWindowTop;
+            let distanceFromBottomToAppear = 200;
+
+            if (elementTopToWindowBottom > distanceFromBottomToAppear) {
+                $(element[i]).addClass('js-fade-element-show');
+            }
+            else if (elementTopToWindowBottom < 0) {
+                $(element[i]).removeClass('js-fade-element-show');
+                $(element[i]).addClass('js-fade-element-hide');
+            }
+        }
+    });
+
+
 })();
