b12eed0236a489e7ac05f9dd079edb2760fd8ae5
Andrei Ivanov <z.shpack@gmail.com>
2016-11-19 16:51:32 -0800

    GSAP added. Intro page animations. Typography updates

---
+++ app/scripts/gsap.js
@@ -0,0 +1,21 @@
+
+
+(function() {
+
+    var elIntroHeader = $('.gsap-intro-header');
+    var elIntroNav = $('.gsap-intro-nav');
+    var elIntroBg = $('.gsap-intro-bg');
+    var tlIntro = new TimelineLite();
+
+    tlIntro.from(elIntroHeader, 0.5, {
+        autoAlpha: 0,
+        y: -50,
+        ease: Power1.easeOut,
+        delay: 0.75
+    }).staggerFrom(elIntroBg, 0.75, {
+        autoAlpha: 0,
+    }, 0.5).from(elIntroNav, 0.5, {
+        autoAlpha: 0
+    }, '+= 3');
+
+})();
